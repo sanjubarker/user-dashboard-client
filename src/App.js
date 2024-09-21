2aa6639dbee5a88d10eee61f28ffe7c9105c4a06
sanjulb <sanjuambhi20@gmail.com>
2024-09-21 18:43:37 +0530

    theme code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './utils/theme';
+import { lightTheme, darkTheme } from './utils/theme';
+import ThemeToggle from './components/ThemeToggle'
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
-  </ThemeProvider>
-);
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    setIsDarkMode(savedTheme === 'dark');
+  }, []);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+    localStorage.setItem('theme', !isDarkMode ? 'dark' : 'light'); // Save preference to local storage
+  };
+
+  const selectedTheme = useMemo(() => (isDarkMode ? darkTheme : lightTheme), [isDarkMode]);
+
+  return (
+    <ThemeProvider theme={selectedTheme}>
+      <Router>
+        <div style={{ padding: '1rem' }}>
+          <ThemeToggle toggleTheme={toggleTheme} />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
 
 export default App;
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,25 @@
 import { createTheme } from '@mui/material/styles';
 
-export const theme = createTheme({
+export const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
   },
 });
